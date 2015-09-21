3f85be33a19eabdf192bbb127f82d7780f129613
Thanh Vui <thanhvui.it@gmail.com>
2015-09-21 19:32:16 +0700

    update

--- app.js
+++ app.js
@@ -14,12 +14,13 @@ var app = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
+app.set('view engine', 'ejs');
+//app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.bodyParser());
+app.use(express.json());
+app.use(express.urlencoded());
+//app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
@@ -27,9 +28,40 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+/*------------------------------------------
+    connection peer, register as middleware
+    type koneksi : single,pool and request 
+-------------------------------------------*/
+//DB config
+/*var mysql      = require('mysql');
+global.connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'nodejs'
+});
 
+connection.connect(function(err){
+	if(!err) {
+    console.log("Database is connected ... \n\n");  
+	} else {
+    console.log("Error connecting database ... \n\n");  
+	}
+});*/
+
+//Customer
+//var customers = require('./routes/customers');
+app.get("/customers",customers.list);
+
+app.get('/', routes.index);//route customer list
+//app.get('/customers', customers.list);//route add customer, get n post
+//app.get('/customers/add', customers.add);
+//app.post('/customers/add', customers.save);//route delete customer
+//app.get('/customers/delete/:id', customers.delete_customer);//edit customer route , get n post
+//app.get('/customers/edit/:id', customers.edit); 
+//app.post('/customers/edit/:id',customers.save_edit);
+
+app.use(app.router);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
